546a95eda3d8b9611bc1b43d118ebe18c988bfe1
Novyk Maksym <maks731maks@gmail.com>
2024-10-31 15:46:52 +0200

    task_04

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ Add a floader topic_02. Add assignments for Calc_case, Calc_if, Discriminant+ an
 
 # 2024-10-16
 Add a floader topic_03. Add assignments for Calc_case(old), Search_function, Test_dictionary and Test_lists.
+
+# 2024-10-31
+Add a floader topic_04. Add assignments for Calc+, Calc++.
---
+++ topic_04/Calc++.py
@@ -0,0 +1,46 @@
+def inp():
+    while True:
+        print("Enter two numbers:")
+        a = input("a: ")
+        b = input("b: ")
+        try:
+            a = float(a)
+            b = float(b)
+            break
+        except ValueError:
+            print("It's not a number.\n")
+    return a, b
+
+def calc(a, b):
+    while True:
+        tool = input("Select a tool: '+' , '-' , '*' , '/' or '^'\n")
+        if tool in ['+', '-', '*', '/', '^']:
+            break
+        else:
+            print("Invalid operator.\n")
+    match tool:
+        case "+":
+            return a + b
+        case "-":
+            return a - b
+        case "*":
+            return a * b
+        case "/":
+            if b == 0:
+                print("Cannot divide by 0")
+            else:
+                return a / b
+        case "^":
+            return a ** b
+        
+exreload = 1
+while exreload != 0:
+    a, b = inp()
+    result = calc(a, b)
+    print("Result:", result)
+    while True:
+        exreload = input("Again? Y/N: ")
+        if exreload.lower().strip() in ['y', 'n', 'yes', 'no']:
+            break
+        else:
+            print("Selection error.\n")
